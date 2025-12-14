9fff3788395009a421232991e76e1cc21048e8e4
clement893 <clement@nukleo.com>
2025-12-13 21:19:39 -0500

    Corriger import startTransition dans main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import { startTransition } from "react";
 import { trpc } from "@/lib/trpc";
 import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";

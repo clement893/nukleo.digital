74cf3f5b9e3e8bd635ef823c07e6496900baaf4d
clement893 <clement@nukleo.com>
2025-12-19 23:31:54 -0500

    CRITICAL FIX: Pre-import useLocalizedPath hook in App.tsx to ensure it's in main chunk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import PageLoader from "./components/PageLoader";
 
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
+// CRITICAL: Pre-import useLocalizedPath to ensure it's in the main chunk
+import "./hooks/useLocalizedPath";
 import ScrollToTop from "./components/ScrollToTop";
 import ArrowBackground from "./components/ArrowBackground";
 import AnalyticsLoader from "./components/AnalyticsLoader";
